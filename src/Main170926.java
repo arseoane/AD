500ecf596897519247e4be2460b4044d21bdc25f
arseoane <arodriguezseoane@danielcastelao.org>
2026-09-17 09:45:06 +0200

    17/09/26

---
+++ src/Main170926.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Main170926 {
+    static class ex1 {
+        File texto1 = new File("/home/dam26/texto1.txt");
+        File texto2 = new File("/home/dam26/texto2.txt");
+
+
+    }
+}
